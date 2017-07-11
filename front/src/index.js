01e9d4af5f5c4bef890076bc7f6fe43ed503e2fa
Wonwoo Choi <chwo9843@gmail.com>
2017-07-11 20:46:42 +0900

    Try basic styling

--- front/src/index.js
+++ front/src/index.js
@@ -3,5 +3,6 @@ import main from './main.md';
 import 'normalize.css/normalize.css';
 import 'github-markdown-css/github-markdown.css';
 import 'font-awesome/css/font-awesome.css';
+import './index.css';
 
 document.getElementById('main').innerHTML = main;
